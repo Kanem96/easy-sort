bd2d207864710f73c1ae191391a451f96f2effea
Kane Milligan <kanemilligan1@gmail.com>
2024-04-01 15:43:31 +0100

    add clerk auth

--- app/(marketting)/header.tsx
+++ app/(marketting)/header.tsx
@@ -1,3 +1,13 @@
+import { Button } from "@/components/ui/button";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import { Loader } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 
@@ -8,6 +18,25 @@ const Header = () => {
         <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
           <Image src="/logo.png" height={120} width={120} alt="logo" />
         </div>
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton
+              mode="modal"
+              afterSignInUrl="/find"
+              afterSignUpUrl="/find"
+            >
+              <Button size="lg" variant="ghost">
+                Sign In
+              </Button>
+            </SignInButton>
+          </SignedOut>
+        </ClerkLoaded>
       </div>
     </header>
   );
