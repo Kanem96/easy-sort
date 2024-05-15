58e3bae31db422b3bf505db1b4aa40a12c83f2f4
Kane Milligan <kane.milligan@glean.co>
2024-05-15 17:33:16 +0100

    add categories

--- app/(main)/find/page.tsx
+++ app/(main)/find/page.tsx
@@ -1,13 +1,18 @@
 "use client";
+
+import Categories from "@/components/categories";
 import EventList from "@/components/eventList";
+import prismadb from "@/lib/prismadb";
 import { useSearchContext } from "@/providers/SearchContextProvider";
 import React from "react";
 
-const FindPage = () => {
+const FindPage = async () => {
   const { searchValue, searchLocation } = useSearchContext();
+  const categories = await prismadb.category.findMany();
 
   return (
     <div className="max-w-[988px] mx-auto w-full flex flex-col lg:flex-row items-center p-4 gap-6">
+      <Categories data={categories} />
       <h1 className="font-bold md:text-[1.75] text-xl">
         {searchValue} near {searchLocation}
       </h1>
